5c7cb8c8b199670b4e5c30476fdb63d6249ca49d
Le Anh Duc <ducvui2003@gmail.com>
2025-05-29 19:58:31 +0700

    add script setup homebrew

---
+++ services/services/scripts/stop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+COMPOSE_FILE=$1
+ENV_FILE=$2
+shift 2
+
+if [ $# -eq 0 ]; then
+  echo "🛑 Stopping ALL services in $COMPOSE_FILE"
+  docker-compose -f "$COMPOSE_FILE" --env-file "$ENV_FILE" stop
+else
+  echo "🛑 Stopping services: $@"
+  docker-compose -f "$COMPOSE_FILE" --env-file "$ENV_FILE" stop "$@"
+fi
---
+++ services/services/scripts/up.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+COMPOSE_FILE=$1
+ENV_FILE=$2
+shift 2
+
+for arg in "$@"; do
+  echo "$arg"
+done
+
+echo "🟢 Using compose file: $COMPOSE_FILE and env: $ENV_FILE"
+docker-compose -f "$COMPOSE_FILE" --env-file "$ENV_FILE" up -d "$@"
