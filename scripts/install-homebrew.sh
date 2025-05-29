954afab99f3370e80295b1da9404d1a273b0bc1c
Le Anh Duc <ducvui2003@gmail.com>
2025-05-29 19:58:31 +0700

    add script setup homebrew

---
+++ scripts/install-homebrew.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e  # Exit on error
+
+echo "📦 Installing dependencies..."
+sudo apt update && sudo apt install -y build-essential procps curl file git
+
+echo "🍺 Installing Homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "✅ Homebrew installed!"
+brew --version
+
+echo "Install Brew file"
+/bin/bash brew bundle --file=~/.dotfile/Brewfile/Brewfile
+
+
---
+++ services/services/scripts/down.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+COMPOSE_FILE=$1
+ENV_FILE=$2
+shift 2
+
+echo "🔴 Stopping services using: $COMPOSE_FILE with env: $ENV_FILE"
+
+if [ "$#" -eq 0 ]; then
+  docker-compose -f "$COMPOSE_FILE" --env-file "$ENV_FILE" down
+else
+  docker-compose -f "$COMPOSE_FILE" --env-file "$ENV_FILE" stop "$@"
+fi
